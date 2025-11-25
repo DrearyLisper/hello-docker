4d1a5aacdbc7b61af2d727724dbf7652dd3e0a4d
Dreary Lisper <drearylisper@gmail.com>
2025-11-25 22:05:53 +0000

    [3\n] Separate CSS and add css reader

--- main.lisp
+++ main.lisp
@@ -14,11 +14,16 @@
 (defun main ()
   (woo:run
    (lambda (env)
-     (declare (ignore env))
-     (let ((machine (machine-instance)))
-       (multiple-value-bind (sec min hour date month year)
-           (get-decoded-time)
-         (list 200 '(:content-type "text/html")
-               (list (format nil (read-template "templates/index.html") machine year month date hour min sec))))))
+     (let ((path (getf env :request-uri)))
+       (cond
+         ((string= path "/style.css")
+          (list 200 '(:content-type "text/css")
+                (list (read-template "templates/style.css"))))
+         (t
+          (let ((machine (machine-instance)))
+            (multiple-value-bind (sec min hour date month year)
+                (get-decoded-time)
+              (list 200 '(:content-type "text/html")
+                    (list (format nil (read-template "templates/index.html") machine year month date hour min sec))) ))) )))
    :port 5000
    :address "0.0.0.0"))
